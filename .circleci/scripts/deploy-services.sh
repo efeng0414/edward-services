fb0d3a8b91e75dd3463802746efe5fa6008fa0bf
efeng0414 <efeng@nobul.com>
2019-05-28 14:25:53 -0400

    wip...

--- .circleci/scripts/deploy-services.sh
+++ .circleci/scripts/deploy-services.sh
@@ -17,16 +17,26 @@ do
 
         if yarn deploy:dev
         then 
-            OLD_VERSION=$(gcloud app versions list --service email-test | awk '($3 -eq 0 && $5 == "STOPPED") {print $2}')
-            gcloud app services delete $SERVICE_NAME --version=$OLD_VERSION
+            OLD_VERSION=$(gcloud app versions list --service $SERVICE_NAME | awk '($3 -eq 0 && $5 == "STOPPED") {print $2}')
+
+            echo "Deleting service $SERVICE_NAME version $OLD_VERSION........."
+
+            [ ! -z "$OLD_VERSION" ] && gcloud app services delete $SERVICE_NAME --version=$OLD_VERSION --quiet
+            echo "$SERVICE_NAME deployed to dev2 successfully!! 💯💯💯"
+            
         else 
             echo "dev2 deployment fail: $f"
         fi
 
         # if yarn deploy:webtesting
         # then 
-        #     OLD_VERSION=$(gcloud app versions list --service email-test | awk '($3 -eq 0 && $5 == "STOPPED") {print $2}')
-        #     gcloud app services delete $SERVICE_NAME --version=$OLD_VERSION
+        #     OLD_VERSION=$(gcloud app versions list --service $SERVICE_NAME | awk '($3 -eq 0 && $5 == "STOPPED") {print $2}')
+
+        #     echo "Deleting service $SERVICE_NAME version $OLD_VERSION........."
+
+        #     [ ! -z "$OLD_VERSION" ] && gcloud app services delete $SERVICE_NAME --version=$OLD_VERSION --quiet
+        #     echo "$SERVICE_NAME deployed to webtesting successfully!! 💯💯💯"
+            
         # else 
         #     echo "webtesting deployment fail: $f"
         # fi
@@ -34,3 +44,6 @@ do
         cd ../..
     fi
 done
+
+
+echo "✅✅ ======= Finish deployment =========== ✅✅"
